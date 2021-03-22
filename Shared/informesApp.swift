96ca354ae827eb7bf9f42cdb94375c069cd7aa25
misael-macAir <hola@landercorp.mx>
2021-03-22 10:28:04 -0600

    Initial Commit

---
+++ Shared/informesApp.swift
@@ -0,0 +1,20 @@
+//
+//  informesApp.swift
+//  Shared
+//
+//  Created by Francisco Misael Landero Ychante on 22/03/21.
+//
+
+import SwiftUI
+
+@main
+struct informesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
